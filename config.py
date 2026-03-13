412d6aa8f5be5f19083d249162aa491317322dd8
Anirudh Iyengar <aniyengar@ucsd.edu>
2026-03-13 05:08:06 -0700

    fix: debug config env reading

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-SUPABASE_URL = os.environ.get("SUPABASE_URL") or os.environ.get("SUPABASE_URL", "")
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY") or os.environ.get("SUPABASE_KEY", "")
+SUPABASE_URL = os.environ.get("SUPABASE_URL") or os.environ.get("SUPABASE_URL", "https://xrvqvrmylwltptyqyjfd.supabase.co")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY") or os.environ.get("SUPABASE_KEY", "sb_publishable_35Mqfnw7XJItQCyTX2vY9Q__m4APn2_")
 
 if not SUPABASE_URL:
     raise ValueError("SUPABASE_URL environment variable is not set")
