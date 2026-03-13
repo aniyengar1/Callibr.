3a045ed2892ea0fc0bb2cbd52be1ccec0ad4e57d
Anirudh Iyengar <aniyengar@ucsd.edu>
2026-03-13 05:03:48 -0700

    fix: config.py reads from env vars

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 
 SUPABASE_URL = os.environ.get("SUPABASE_URL", "https://xrvqvrmylwltptyqyjfd.supabase.co")
-SUPABASE_KEY = os.environ.get("sb_publishable_35Mqfnw7XJItQCyTX2vY9Q__m4APn2_")
-
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "sb_publishable_35Mqfnw7XJItQCyTX2vY9Q__m4APn2_")
